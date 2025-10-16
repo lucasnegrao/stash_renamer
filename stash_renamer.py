2625e247140e6c824daee1efcc2e93a8d46c96f9
Lucas Negrao <lucas.negrao@gmail.com>
2025-10-16 01:45:27 -0300

    group

--- stash_renamer.py
+++ stash_renamer.py
@@ -388,7 +388,7 @@ query findScenes($filter: FindFilterType!, $scene_filter: SceneFilterType!) {
       studio { name }
       performers { name gender }
       tags { name }
-      groups { sceneGroup { Group{ id name } } }
+      groups {  Group{ id name }  }
     }
   }
 }
